f76123b5dd5be5f7807368e2ba42a14a8816af60
umar200000 <ruzievumar19@gmail.com>
2024-01-17 16:15:06 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:umar1/tools.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+
+  int result = 0;
+
+  Widget bottoms(int index) {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue.shade300,
+        ),
+        onPressed: () {
+          setState(() {
+            data[counter]["Answer"][index]["check"] ? result++ : 0;
+            counter++;
+          });
+        },
+        child: Text("${data[counter]["Answer"][index]["option"]}",
+            style: const TextStyle(fontSize: 20, color: Colors.black)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.purple.shade300,
+            title: const Text("English Quiz"),
+          ),
+          body: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: counter < data.length
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          "${data[counter]["Question"]}",
+                          style: const TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                        bottoms(0),
+                        bottoms(1),
+                        bottoms(2),
+                        bottoms(3),
+                      ],
+                    )
+                  : Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Result: $result/${data.length}",
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 25),
+                          ),
+                          ElevatedButton.icon(
+                            onPressed: () {
+                              setState(() {
+                                counter = 0;
+                                result = 0;
+                              });
+                            },
+                            icon: const Icon(Icons.restart_alt),
+                            label: const Text("RESTART"),
+                          )
+                        ],
+                      ),
+                    )),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/tools.dart
@@ -0,0 +1,47 @@
+List<Map<String, dynamic>> data = [
+  {
+    "Question": "What ____ your name?",
+    "Answer": [
+      {"option": "are", "check": false},
+      {"option": "is", "check": true},
+      {"option": "am", "check": false},
+      {"option": "do", "check": false},
+    ]
+  },
+  {
+    "Question": "Who ____ you?",
+    "Answer": [
+      {"option": "does", "check": false},
+      {"option": "am", "check": false},
+      {"option": "do", "check": false},
+      {"option": "are", "check": true},
+    ]
+  },
+  {
+    "Question": "How ____ are you?",
+    "Answer": [
+      {"option": "old", "check": true},
+      {"option": "can", "check": false},
+      {"option": "make", "check": false},
+      {"option": "is", "check": false},
+    ]
+  },
+  {
+    "Question": "____ she ____ Student?",
+    "Answer": [
+      {"option": "does/a", "check": false},
+      {"option": "am/an", "check": false},
+      {"option": "is/a", "check": true},
+      {"option": "are/an", "check": false},
+    ]
+  },
+  {
+    "Question": "do you ____ Lipton?",
+    "Answer": [
+      {"option": "think", "check": false},
+      {"option": "am", "check": false},
+      {"option": "do", "check": false},
+      {"option": "like", "check": true},
+    ]
+  },
+];
